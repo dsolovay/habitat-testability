554d1b90a36ffa6e6fba23d58bf3684b2cb6c264
dsolovay <dan.solovay@velir.com>
2016-08-06 21:53:22 -0400

    Add publish profile
    Point to new Breadcrumb
    
    This fails when http://habitat.dev.local/About-Habitat/Introduction
    is loaded, because we haven't yet wired up an Autofac module.
    
    Add autofac

---
+++ src/Feature/Navigation/glass.demo/Sitecore.Feature.Navigation.GlassDemo/Modules/AutofacModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Feature.Navigation.GlassDemo.Modules
+{
+  using Autofac;
+  using Autofac.Integration.Mvc;
+  using Glass.Mapper.Sc;
+  using Sitecore.Feature.Navigation.GlassDemo.Controllers;
+
+  public class AutofacModule:Module
+  {
+    protected override void Load(ContainerBuilder builder)
+    {
+      builder.RegisterType<SitecoreContext>().As<ISitecoreContext>();
+
+      builder.RegisterControllers(typeof(NavigationGlassController).Assembly);
+    }
+  }
+}
