9bbfdd19c90af51c40a5b18603be64dfafbf7f00
dsolovay <dan.solovay@velir.com>
2016-08-07 01:03:16 -0400

    Add ID to Navigable

--- src/Feature/Navigation/glass.demo/Sitecore.Feature.Navigation.GlassDemo.Tests/NavigableTests.cs
+++ src/Feature/Navigation/glass.demo/Sitecore.Feature.Navigation.GlassDemo.Tests/NavigableTests.cs
@@ -1,5 +1,6 @@
 namespace Sitecore.Feature.Navigation.GlassDemo.Tests
 {
+  using System;
   using System.Reflection;
   using FluentAssertions;
   using Sitecore.Feature.Navigation.GlassDemo.Models;
@@ -13,9 +14,11 @@ namespace Sitecore.Feature.Navigation.GlassDemo.Tests
       navItem.Url.GetType().Should().Be<string>("because this stores the link");
       navItem.IsActive.GetType().Should().Be<bool>("because this toggles a CSS class");
       navItem.NavigationTitle.GetType().Should().Be<string>("because this is rendered text");
+      navItem.Id.GetType().Should().Be<Guid>();
+
       PropertyInfo property = typeof(Navigable).GetProperty("Parent");
       property.Should().BeVirtual("because Glass requires this so it can use lazy loading and avoid infinite recursion");
-      property.PropertyType.Should().Be<Navigable>("because we need to crawl up the tree")
+      property.PropertyType.Should().Be<Navigable>("because we need to crawl up the tree");
     }
   }
 }
--- src/Feature/Navigation/glass.demo/Sitecore.Feature.Navigation.GlassDemo/Models/Navigable.cs
+++ src/Feature/Navigation/glass.demo/Sitecore.Feature.Navigation.GlassDemo/Models/Navigable.cs
@@ -1,10 +1,15 @@
 namespace Sitecore.Feature.Navigation.GlassDemo.Models
 {
+  using System;
+  using Glass.Mapper.Sc.Configuration.Attributes;
+
   public class Navigable
   {
+    [SitecoreParent]
     public virtual Navigable Parent { get; set; }
     public string Url { get; set; }
     public bool IsActive { get; set; }
     public string NavigationTitle { get; set; }
+    public Guid Id { get; set; }
   }
 }
