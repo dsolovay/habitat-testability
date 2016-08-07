b50993932e09254bcfef12222ca3823bcd0af222
dsolovay <dan.solovay@velir.com>
2016-08-07 00:28:31 -0400

    green

--- src/Feature/Navigation/glass.demo/Sitecore.Feature.Navigation.GlassDemo/Models/Navigable.cs
+++ src/Feature/Navigation/glass.demo/Sitecore.Feature.Navigation.GlassDemo/Models/Navigable.cs
@@ -2,7 +2,7 @@ namespace Sitecore.Feature.Navigation.GlassDemo.Models
 {
   public class Navigable
   {
-    public Navigable Parent { get; set; }
+    public virtual Navigable Parent { get; set; }
     public string Url { get; set; }
     public bool IsActive { get; set; }
     public string NavigationTitle { get; set; }
