07c887b75ebf472d8a3643f1d271ec68a1a7578f
dsolovay <dan.solovay@velir.com>
2016-08-16 18:11:46 -0400

    Add ContactFacet projects with commented out tests

---
+++ src/Feature/ContactFacets/tests/Sitecore.Feature.ContactFacets.Tests/Controllers/ContactFacetControllerTests.cs
@@ -0,0 +1,29 @@
+﻿namespace Sitecore.Feature.ContactFacets.Tests.Controllers
+{
+  using System.Web.Mvc;
+  using FluentAssertions;
+  using Xunit;
+
+  public class ContactFacetControllerTests
+  {
+
+    //[Fact]
+    //public void New_Called_ReturnsController()
+    //{
+
+    //  var sut = new ContactFacetController();
+
+    //  sut.Should().NotBeNull();
+    //}
+
+    //[Fact]
+    //public void Index__CalledWithNoParms_ReturnsView()
+    //{
+    //  var sut = new ContactFacetController();
+
+    //  ActionResult result = sut.Index();
+
+    //  result.Should().BeOfType<ViewResult>();
+    //}
+  }
+}
